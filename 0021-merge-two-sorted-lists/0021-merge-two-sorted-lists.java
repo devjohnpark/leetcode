504825aafad3845c624a19783f6dc50b8be26725
John Park <devjohnpark@gmail.com>
2025-05-07 10:05:29 +0900

    Time: 0 ms (100%), Space: 42.7 MB (34.01%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -32,4 +32,18 @@ class Solution {
 
         return list.next; // head of list val is initialize 0
     }
+
+    // recursive version
+    // public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    //     if (list1 == null) return list2;
+    //     if (list2 == null) return list1;
+
+    //     if (list1.val < list2.val) {
+    //         list1.next = mergeTwoLists(list1.next, list2);
+    //         return list1;
+    //     } else {
+    //         list2.next = mergeTwoLists(list2.next, list1);
+    //         return list2;
+    //     } 
+    // }
 }
