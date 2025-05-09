5fd2a09e4460f813f1391215c81cdb5ba571990d
John Park <devjohnpark@gmail.com>
2025-05-09 09:09:41 +0900

    Time: 0 ms (100%), Space: 44.6 MB (50.06%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -10,9 +10,9 @@
  * }
  */
 
- // 원형에서 속도 차이를 사용해서 접점을 발생시켜 원형인지 확인 가능
- // Using two pointer
- // speed of pointer2 x 2 -> pointer 1 reach pointer 2
+// 지점으로 푸는 문제: 원형에서 속도 차이를 사용해서 접점을 발생시켜 원형인지 확인 가능
+// Using two pointer
+// speed of pointer2 x 2 -> pointer 1 reach pointer 2
 public class Solution {
     public boolean hasCycle(ListNode head) {
         if (head == null) { return false; }
