a2a2f80771342f87af918a5472469cf0a7f700df
John Park <devjohnpark@gmail.com>
2025-05-27 08:21:37 +0900

    Time: 2 ms (98.07%), Space: 43.4 MB (67.78%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,18 +4,28 @@ class Solution {
         if (s.length() != t.length()) {
             return false;
         }
-        
+
         int size = 'z' - 'a' + 1;
         int[] arr = new int[size];
-
-        for (int i = 0; i < s.length(); i++) {
-            arr[s.charAt(i) - 'a'] += 1;
+ 
+        // String.charAt(index)는 매번 호출이 필요해서 시간 더 걸림
+        // String.toCharArray() 한번의 호출로 배열로 변환하는 것이 더 빠름
+        for (char c: s.toCharArray()) {
+            arr[c - 'a'] += 1;
         }
 
-        for (int i = 0; i < t.length(); i++) {
-            arr[t.charAt(i) - 'a'] -= 1;
+        // for (int i = 0; i < s.length(); i++) {
+        //     arr[s.charAt(i) - 'a'] += 1;
+        // }
+
+        // for (int i = 0; i < t.length(); i++) {
+        //     arr[t.charAt(i) - 'a'] -= 1;
+        // }
+
+        for (char c: t.toCharArray()) {
+            arr[c - 'a'] -= 1;
         }
-        
+
         for (int n: arr) {
             if (n != 0) {
                 return false;
