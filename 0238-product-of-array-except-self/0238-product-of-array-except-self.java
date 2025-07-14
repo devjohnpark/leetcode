e3ca242106504ef13c6d4c977b6018044d9cb95b
John Park <devjohnpark@gmail.com>
2025-07-14 11:47:15 +0900

    Time: 1 ms (99.66%), Space: 55.2 MB (87.46%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,12 +1,13 @@
 class Solution {
     // 문제 정의
     // answer[i]: 자기 자신을 제외한 모든 배열 요소의 곱셈을 반환
-    // O(n) 내에 동작: for 문으로 n x n -1 연산을 수행하면 안됨
+    // O(n) 내에 동작: for 문으로 n x n -1 연산을 수행하면 안됨 -> 배열 조회의 시간복잡도인 O(n) 내에 연산 해야한다.
 
     // 핵심 알고리즘
     // 접근법 부터 다시 생각해야한다.
     // 반복문 1: 출력할 배열에 주어진 배열 요소의 왼쪽의 요소들을 모두 곱한다.
     // 반복문 2: 출력할 배열에 주어진 배열 요소의 오른쪽 요소들을 모두 곱한다.
+    // 반복문 1: O(n) + 반복문 2: O(n) => O(n)
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] answer = new int[n];
