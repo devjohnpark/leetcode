49f3a793b7e563e2b0034a6db0a9ee30b7dbfee4
John Park <devjohnpark@gmail.com>
2025-07-31 09:49:22 +0900

    Time: 2 ms (72.27%), Space: 62 MB (18.86%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -11,9 +11,9 @@ class Solution {
         int min = prices[0];
         int n = prices.length;
         for (int i = 1; i < n; i++) {
-            maxProfit = Math.max(prices[i] - min, maxProfit); 
-            min = Math.min(prices[i], min);
+            maxProfit = Math.max(prices[i] - min, maxProfit); // 1. 배열을 순회하면서 이익 최대값 업데이트
+            min = Math.min(prices[i], min); // 2. 배열을 순회하면서 요소값중 최솟값 업데이트
         }
-        return maxProfit;
+        return maxProfit; // 3. 이익 최댓값 반환
     }
 }
