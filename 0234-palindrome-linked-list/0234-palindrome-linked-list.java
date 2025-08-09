d14a9eed4d7d5b9ca85be59e76b9c6eb61621431
John Park <devjohnpark@gmail.com>
2025-08-10 00:55:30 +0900

    Time: 3 ms (99.73%), Space: 69.4 MB (7.65%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,14 +10,14 @@
  */
 
 
-// 1 3 3 4 3 3 1
-
+// 1 -> 2 -> 2 -> 1
 // 1. 반복문
-// 리스트 역순 정렬
-// 중간 노드 찾기
+// 중간 노드를 찾으며 리스트 역순 정렬
+// 1) 중간 노드 찾기: 속도 2배 차이나는 포인터를 사용 (2칸씩 이동하는 포인터와 1칸씩 이동하는 포인터)
+// 2) 리스트 역순 정렬: 중간 노드까지 역순 정렬 (1 <- 2, 2 -> 1)
 
 // 2. 반목분
-// 중간 노드 부터 값 동일한지 비교한 후에 다르다면 false 반환
+// 중간 노드 부터 혹은 중간 노드 빼고, 값 동일한지 비교한 후에 다르다면 false 반환
 class Solution {
     public boolean isPalindrome(ListNode head) {    
         ListNode slow = head;
@@ -25,29 +25,22 @@ class Solution {
         ListNode prev = null;
         while(fast != null && fast.next != null && slow != null) {
             fast = fast.next.next;
-
-            // 1 2 3 4 3 2 1
             // slow 역순 연결
-            // 1. 현재 노드애 연결된 다음 노드를 임시 저장
-            // 2. 현재 노드를 이전 노드로 연결
-            // 3. 이전 노드를 현재 노드로 업데이트
-            // 4. 현재 노드를 임시로 저장한 다음 노드로 업데이트
-            ListNode tmp = slow.next;
+            ListNode next = slow.next;
             slow.next = prev;
             prev = slow;
-            slow = tmp;
+            slow = next;
         }
-        // now slow is middle
 
         // 1 2 3 2 1
-        // 홀수시 slow 인덱스는 2번 인덱스를 가리키고 fast != null이다.
+        // 노드의 개수가 홀수시 fast != null이며, slow 인덱스를 3번에서 2번으로 이동시킨다.
         if (fast != null) {
             slow = slow.next; // move next node
         }
 
         // 1 2 2 1
-        // 짝수시 slow 2번 인덱스를 가리키고 fast == null이다.
-
+        // slow 2 -> 1
+        // prev 2 -> 1
         while(prev != null && slow != null) {
             if (prev.val != slow.val) {
                 return false;
