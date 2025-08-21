f306ea6c07adb777a461c78a5ea87aa394dc03c4
John Park <devjohnpark@gmail.com>
2025-08-21 10:40:37 +0900

    Time: 21 ms (92.84%), Space: 51 MB (66.95%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -9,6 +9,8 @@ class Solution {
     //         int distB = b[0] * b[0] + b[1] * b[1];
     //         return Integer.compare(distA, distB);
     //     });
+    //     // int[k][2] 배열을 할당 → 공간 O(k)
+    //     // k개의 int[] 참조 복사 -> 시간 O(k)
     //     return Arrays.copyOfRange(points, 0, k); // O(k) 복사 비용 발생
     // }
 
@@ -18,20 +20,20 @@ class Solution {
     // 그러면 O(log(k+1)) x O(n) = O(nlogk)에 문제를 해결할수있다.
     public int[][] kClosest(int[][] points, int k) {
         // int 배열을 저장하는 우선순위 큐
-        // x[0]^2 + x[1]^2을 내림차순으로 정렬
+        // x[0]^2 + x[1]^2을 내림차순으로 정렬: b[0]^2 - a[0]^2하면 내림차순 정렬
         // PriorityQueue는 Object[]로 저장한다.
         PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
             (a, b) -> (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1])
         );
 
-        // 이차원 배열 내의 배열 순회
+        // 총 시간복잡도: O(nlogk)
+        // 이차원 배열 내의 배열 순회: O(n)
         for (int[] point: points) {
-            maxHeap.add(point);
-            if (maxHeap.size() > k) maxHeap.remove(); // 저장한 크기가 k개 보다 크면 최대값을 삭제
+            maxHeap.add(point); // 최대 삽입 연산: O(logk)
+            if (maxHeap.size() > k) maxHeap.remove(); // 저장한 크기가 k개 보다 크면 최대값을 삭제: O(logk)
         }
 
-        // k개 만큼 저장하는 배열 (오름 차순 저장을 안해도됨)
-        // O(klogk)
+        // k개 만큼 저장하는 배열 (오름 차순으로 저장 안해도됨)
         // int[][] result = new int[k][2];
         // for (int i = 0; i < k; i++) {
         //     result[i] = maxHeap.remove(); 
@@ -43,8 +45,8 @@ class Solution {
         // 정렬되어서 반환되는 것이 아니라, 단지 힙에 들어있는 원소들을 배열로 모은 것이다. 
         // PriorityQueue는 Object[]로 저장하기 때문에 배열로 변환시에 O(k)가 걸리지만, 삭제 연산하면 O(klogk)가 걸린다.
         // new int[0][0]: 타입만 알려주고 크기는 모를 때 유용, 여기서는 반환할 배열이 int[k][2] 이므로 이렇게 지정
-        // int[][] 배열을 할당 → 공간 O(k)
-        // int[] 참조 복사 -> 시간 O(k)
+        // int[k][2] 배열을 할당 -> 공간 O(k)
+        // k개의 int[] 참조 복사 -> 시간 O(k)
         return maxHeap.toArray(new int[k][2]); 
     }
 }
