9ea27e05e524fedd33db9a899dd6691c877b6a99
John Park <devjohnpark@gmail.com>
2025-08-29 09:48:52 +0900

    Time: 4 ms (91.11%), Space: 44.9 MB (43.79%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -7,28 +7,28 @@ class Solution {
     // case: pwwkew -> pw, wke, w에서 
     // edge case: lopwwkpew -> lopw, wkpe에서 각 substring 이전에 중복된 문자는 제거해야한다.
 
-    // 2n -> O(n)
-    // O(k) < O(n) -> 중복 없는 문자의 수 k (HashSet에 저장하는 값의 개수)
-    public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        int left = 0, maxLen = 0;
-        HashSet<Character> set = new HashSet<>();
+    // time complexcity 2n -> O(n)
+    // space complexcity: 중복 없는 문자의 수 k -> O(k)
+    // public int lengthOfLongestSubstring(String s) {
+    //     int n = s.length();
+    //     int left = 0, maxLen = 0;
+    //     HashSet<Character> set = new HashSet<>();
         
-        for (int right = 0; right < n; right++) {
-            char c = s.charAt(right);
+    //     for (int right = 0; right < n; right++) {
+    //         char c = s.charAt(right);
             
-            // 중복 문자가 있으면 left를 이동시켜 중복 제거
-            while (set.contains(c)) {
-                set.remove(s.charAt(left));
-                left++;
-            }
+    //         // 중복 문자가 있으면 left를 이동시켜 중복 제거
+    //         while (set.contains(c)) {
+    //             set.remove(s.charAt(left));
+    //             left++;
+    //         }
             
-            set.add(c);
-            maxLen = Math.max(maxLen, right - left + 1);
-        }
+    //         set.add(c);
+    //         maxLen = Math.max(maxLen, right - left + 1);
+    //     }
         
-        return maxLen;
-    }
+    //     return maxLen;
+    // }
 
     // // 슬라이딩 윈도우 + HashMap (최적화된 버전)
     // public int lengthOfLongestSubstring(String s) {
@@ -50,4 +50,52 @@ class Solution {
         
     //     return maxLen;
     // }
+
+
+
+
+
+
+    // 문제 정의: 주어진 문자여에서 중복되지 않는 문자로 구성된 가장긴 substring을 구해라. -> 연속된 문자열에서 동일한 문자가 있으면 안된다.
+    // 시간복잡도: 50000이므로 O(nlogn) 이내로 풀어야한다.
+    // 부르트 포스
+    // 문자열의 문자를 순회중에 
+
+    // 핵심 문제 풀이
+    // 문자열 구간 지정
+    // 중복 확인을 위해 문자를 순회하면서 hashmap에 <문자, 인덱스> 저장
+    // 오른쪽 포인터를 이동하면서 hashmap에서 중복되는 문자가 있는지 확인
+    // hashmap에서 가져온 문자의 인덱스가 왼쪽 포인터보다 작으면 true
+    // hashmap에서 가져온 문자의 인덱스가 왼쪽 포인터보다 작으면 false -> 구간 변경 -> 왼쪽 포인터를 현재 지정된 인덱스로 이동, 오른쪽 포인터는 왼쪽 포인터 +1
+    // 문제 다시 봐야됨
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int length = s.length();
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        int maxLen = 1;
+        for (int right = 0; right < length; right++) {
+            // maxLen = Math.max(maxLen, right - left + 1); 
+            char c = s.charAt(right);
+            Integer idx = map.get(c);
+            if (idx != null && idx >= left) {
+                // 구간 업데이트, rigth가 아니라 hashmap에 중복 저장된 인덱스의 바로 다음 인덱스부터 left로 시작해야한다.
+                // left = right;
+                left = idx + 1;
+            } 
+            // 다음 구간에서 중복되는 값 찾기 위해 저장
+            map.put(c, right);
+            maxLen = Math.max(maxLen, right - left + 1); 
+        }
+        return maxLen;
+    }
+
+
+
+
+
+
+
+
+
 }
