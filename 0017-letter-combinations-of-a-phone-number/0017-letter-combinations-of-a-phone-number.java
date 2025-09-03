47ae2c2a5339ae9ada7d02c979cf1b4ba92f6a6e
John Park <devjohnpark@gmail.com>
2025-09-04 08:58:09 +0900

    Time: 0 ms (100%), Space: 42.4 MB (26.7%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,13 +1,14 @@
-// 문제 정의: digit에 매칭되는 String 문자열들을 주어진 String의 길이만큼 조합한 문자열을 반환해라.
-// 입력 크기: digit에 매칭되는 문자열이 최대 길이 ^ digits의 길이 = 4 ^ digits의 길이 = 4 ^ n -> 한번의 탐색으로 풀어야한다.
-// 문제 풀이 과정
-// 다시 완벽 정리 필요 (현재 그래프 탐색 문제 경험이 없어서 굉장히 약함)
+// 문제 정의: 문자열 digits에서 매핑되는 문자열의 조합을 구해라. 단, 문자열 조합의 길이는 digits의 길이만큼이다.
+// 시간 복잡도: 문자간의 조합이므로 digits.length ^ digit 최대 문자열 길이 = 4^4이다.
+// 문제 풀이:
+// 문자를 노드로 치면, 깊이 우선 탐색을 하면서 문자를 StringBuilder에 추가하면서 digits.length에 도달한 문자열을 생성하고 반환 리스트에 추가하면된다.
+// 단, 자식 끝노드 까지 탐색후 다시 올라가서 형제 서브 트리를 탐색해야하므로 깊이 우선 탐색 후에 StringBuilder의 문자를 한개씩 제거해줘야한다.
 class Solution {
     public List<String> letterCombinations(String digits) {
-        List<String> ans = new ArrayList<>(); // 반혼값을 저장할 문자열 리스트: 그래프 탐색으로 만들어진 문자열 조합을 추가
+        List<String> ans = new ArrayList<>();
         if (digits == null || digits.isEmpty()) return ans;
-
-        Map<Character, String> digitToLetters = new HashMap<>();
+        // 인덱스 2부터 시작하므로 편리하게 해시맵으로 매핑된 문자열을 생성
+        HashMap<Character, String> digitToLetters = new HashMap<>();
         digitToLetters.put('2', "abc");
         digitToLetters.put('3', "def");
         digitToLetters.put('4', "ghi");
@@ -16,31 +17,27 @@ class Solution {
         digitToLetters.put('7', "pqrs");
         digitToLetters.put('8', "tuv");
         digitToLetters.put('9', "wxyz");
-
-        dfs(digits, 0, new StringBuilder(), ans, digitToLetters);  // 문자열 조합을 위한 dfs 탐색 시작
+        backtracking(digits, 0, new StringBuilder(), ans, digitToLetters);
         return ans;
     }
 
-    // digits: "23"
-    // idx: "23" 문자열의 인덱스
-    // path: 탐색한 경로를 나타내는 조합 문자열: "ab"(a->d), "ae"(a->e), "af"(a->f), "bd"(b->d), "be"(b->e), "bf"(b->f), ...
-    private void dfs(String digits, int idx, StringBuilder path, List<String> ans, Map<Character, String> digitToLetters) {
-        // 1. 종료 조건: 모든 숫자를 처리했을 때 (digits.length 문자열 path가 완성됬을때)
-        if (idx == digits.length()) { /// idx가 순차적으로 커지면서 
-            ans.add(path.toString()); // 반환 리스트에 생성한 문자열 추가 (그래프 탐색으로 문자를 추가해서 생성한 문자열)
+    // backtracking 재귀 메서드
+    // "23"일때 두번을 반복했다고 인지해야한다. 따라서 깊이(depth)를 0부터 시작해서 digits.length이면 멈춘다.
+    private void backtracking(String digits, int depth, StringBuilder path, List<String> ans, Map<Character, String> digitToLetters) {
+        // 재귀 호출 종료 조건
+        if (depth == digits.length()) {
+            ans.add(path.toString());
             return;
         }
 
-        // 2. 현재 숫자에 해당하는 문자들 가져오기
-        String letters = digitToLetters.get(digits.charAt(idx)); // "abc" = digitToLetters.get('2')
+        // 현재 숫자에 매핑된 문자열 가져오기
+        String letters = digitToLetters.get(digits.charAt(depth)); // "abc" = digitToLetters.get(2)
 
-        // 3. 가능한 문자들을 하나씩 붙이고 DFS 탐색
-        for (int i = 0; i < letters.length(); i++) {  // "abc"의 문자 순회
-            path.append(letters.charAt(i)); // 경로 확장: "ad"(a->d), "ae"(a->e), "af"(a->f), "bd"(b->d), "be"(b->e), "bf"(b->f), ...
-            // System.out.println(path); // "ad", "ae", "af", "bd", "be", "bf", ...
-            dfs(digits, idx + 1, path, ans, digitToLetters); // 다음 digit 탐색 ('2' -> '3')
-            path.deleteCharAt(path.length() - 1); // 경로 되돌리기: "ab"(a->d) -> "a" -> "ae"(a -> e) ...
-            // System.out.println(path); // a, a, a, b, b, b, ...
+        // digits와 매핑되는 문자열을 순회하면서 문자 하나당, 깊이 탐색 재귀 호출로 다음 인덱스를 호출
+        for (int i = 0; i < letters.length(); i++) {
+            path.append(letters.charAt(i)); // 탐색 전 문자 추가 -> 경로 확장: "ad"(a->d), "ae"(a->e), "af"(a->f), "bd"(b->d), "be"(b->e), "bf"(b->f), ...
+            backtracking(digits, depth + 1, path, ans, digitToLetters); // 문자 경로에 추가후에 깊이 탐색 
+            path.deleteCharAt(path.length() - 1); // 탐색 후 문자 삭제 -> 경로 되돌리기: "ab"(a->d) -> "a" -> "ae"(a -> e) ...
         }
     }
 }
