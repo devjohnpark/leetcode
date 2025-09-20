f861b1fec693fba61c0792cdb28a12d89b39a009
John Park <devjohnpark@gmail.com>
2025-09-21 08:41:55 +0900

    Time: 2 ms (97.14%), Space: 55.8 MB (94.79%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,20 +1,30 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int n = gas.length;
-        int fuel  = 0;   
-        int start = 0; 
 
+        // 전체 연료 양이 전체 비용보다 적으면 애초에 불가능
+        int totalFuel = 0;
         for (int i = 0; i < n; i++) {
-            fuel += gas[i] - cost[i];
+            totalFuel += gas[i] - cost[i];
         }
+        if (totalFuel < 0) return -1;
 
-        if (fuel < 0) return -1;
-
-        fuel = 0;
-
+        // 유일한 해가 있는 경우
+        int fuel = 0;
+        int start = 0; 
         for (int i = 0; i < n; i++) {
             fuel  += gas[i] - cost[i];
-            if (fuel < 0) {
+
+            // 현재까지의 출발점은 모두 불가능: i까지 오는 경로에서 이미 연로가 부족하면 그 이전에 시작해도 반드시 실패한다.
+            // 시작점 = 0, 누적 연료 적자 지점 = 2일때 실패
+            // 시작점 = 1, 누적 연료 적자 지점 = 2일때 실패
+            // diff = [ 1, 4, -6, ... ]
+            // 누적 = [ 1, 5, -1, ... ] 
+
+            // 최초 0에서 부족
+            // diff = [ -1, +4, -4, ... ]
+            // 누적 = [ -1, 3, -1, ... ] 
+            if (fuel < 0) { 
                 start = i + 1;
                 fuel = 0; 
             }
