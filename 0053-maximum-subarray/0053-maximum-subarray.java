b70b5325a14cf1cac3e82ac0eb5938ab64360647
John Park <devjohnpark@gmail.com>
2025-09-23 10:35:57 +0900

    Time: 1 ms (99.46%), Space: 57.6 MB (8.22%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -10,10 +10,10 @@
 // 총합이 최대값 보다 크면 최대값을 업데이트한다.
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum = 0;
+        int sum = nums[0];
         int n = nums.length;
-        int max = nums[0];
-        for (int i = 0; i < n; i++) {
+        int max = sum;
+        for (int i = 1; i < n; i++) {
             if (sum < 0) sum = 0;
             sum += nums[i];
             max = Math.max(sum, max);
